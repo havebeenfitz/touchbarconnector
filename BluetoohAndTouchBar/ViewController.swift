cf7b382b08a70bd8fa51e2e4b849e4bc1cb68221
Max Kraev <havebeenfitz@maxs-macbook-pro.local>
2018-05-17 00:40:05 +0500

    Initial Commit

---
+++ BluetoohAndTouchBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BluetoohAndTouchBar
+//
+//  Created by Max Kraev on 17/05/2018.
+//  Copyright © 2018 Max Kraev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
