d6ce8aa07cca69e9abb49554137353ef42917b67
Max kraev <max.kraev@gmail.com>
2020-03-24 11:32:16 +0300

    Clean up

--- BluetoohAndTouchBar/AppDelegate.swift
+++ BluetoohAndTouchBar/AppDelegate.swift
@@ -11,27 +11,22 @@ import Cocoa
 @NSApplicationMain
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+    func applicationDidFinishLaunching(_: Notification) {
         guard let button = statusItem.button else { return }
-        button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+        button.image = NSImage(named: NSImage.Name("StatusBarButtonImage"))
         button.action = #selector(applicationShouldHandleReopen(_:hasVisibleWindows:))
         NSApp.setActivationPolicy(NSApplication.ActivationPolicy.regular)
         NSApp.activate(ignoringOtherApps: true)
         constructMenu()
     }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        
-    }
 
-    func applicationDidHide(_ notification: Notification) {
-        
-    }
-    
-    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    func applicationWillTerminate(_: Notification) {}
+
+    func applicationDidHide(_: Notification) {}
+
+    func applicationShouldHandleReopen(_: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
         if flag {
             NSApp.windows.last?.makeKeyAndOrderFront(self)
             return true
@@ -39,8 +34,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             return false
         }
     }
-    
-    //MARK: Custom methods
+
+    // MARK: Custom methods
 
     func constructMenu() {
         let menu = NSMenu()
@@ -48,14 +43,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         menu.addItem(NSMenuItem(title: "Show", action: #selector(NSApplicationDelegate.applicationShouldHandleReopen(_:hasVisibleWindows:)), keyEquivalent: "w"))
         menu.addItem(NSMenuItem(title: "Hide", action: #selector(NSApplication.hide(_:)), keyEquivalent: "e"))
         menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-        
+
         statusItem.menu = menu
     }
-    
+
     @objc func launchFromTray() {
         NSApp.unhide(self)
         NSApp.activate(ignoringOtherApps: true)
     }
-    
-
 }
--- BluetoohAndTouchBar/ViewController.swift
+++ BluetoohAndTouchBar/ViewController.swift
@@ -7,302 +7,258 @@
 //
 
 import Cocoa
+import ColorSync
 import IOBluetooth
 import IOBluetoothUI
-import ColorSync
 
-//MARK: Enums
+// MARK: Enums
 
-fileprivate enum CellIdentifiers {
+private enum CellIdentifiers {
     static let NameCell = "NameCellID"
 }
 
-//MARK:- Controller
+// MARK: - Controller
 
 class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource,
-                      NSScrubberDelegate, NSScrubberDataSource,
-                      IOBluetoothDeviceInquiryDelegate, IOBluetoothDevicePairDelegate {
-   
-    //MARK:- Outlets
-    
-    @IBOutlet weak var clipView: NSClipView!
-    @IBOutlet weak var tableView: NSTableView!
-    @IBOutlet weak var scrubber: NSScrubber!
-    @IBOutlet weak var progressIndicator: NSProgressIndicator!
-    
-    //MARK: Vars
-    
+    NSScrubberDelegate, NSScrubberDataSource,
+    IOBluetoothDeviceInquiryDelegate, IOBluetoothDevicePairDelegate {
+    // MARK: - Outlets
+
+    @IBOutlet var clipView: NSClipView!
+    @IBOutlet var tableView: NSTableView!
+    @IBOutlet var scrubber: NSScrubber!
+    @IBOutlet var progressIndicator: NSProgressIndicator!
+
+    // MARK: Vars
+
     var ioDevices = [IOBluetoothDevice]()
     let ioBluetoothInquiryManager = IOBluetoothDeviceInquiry()
     let pairingDevice = IOBluetoothDevicePair()
 
-    var timer: Timer? = nil
-    
+    var timer: Timer?
+
     var scrubberDeviceItemID = NSUserInterfaceItemIdentifier("Device")
-    
-    //MARK: LifeCycle
-    
+
+    // MARK: LifeCycle
+
     override func viewDidLoad() {
-        
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
-        
+
         scrubber.delegate = self
         scrubber.dataSource = self
         scrubber.register(NSScrubberTextItemView.self, forItemIdentifier: scrubberDeviceItemID)
-        //scrubber.register(NSScrubberImageItemView.self, forItemIdentifier: scrubberDeviceItemID)
-        
+
         ioBluetoothInquiryManager.searchType = kIOBluetoothDeviceSearchLE.rawValue
         ioBluetoothInquiryManager.updateNewDeviceNames = true
         ioBluetoothInquiryManager.delegate = self
         pairingDevice.delegate = self
-        
+
         ioBluetoothInquiryManager.start()
         progressIndicator.startAnimation(self)
         loadPairedDevices()
-        
-        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(repeatScan), userInfo: nil, repeats: true)
 
-        
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(repeatScan), userInfo: nil, repeats: true)
     }
-    
+
     override func viewDidDisappear() {
         timer?.invalidate()
     }
 
-    //MARK:- Tableview delegate and datasource
-    
-    func numberOfRows(in tableView: NSTableView) -> Int {
+    // MARK: - Tableview delegate and datasource
+
+    func numberOfRows(in _: NSTableView) -> Int {
         return ioDevices.count
     }
-    
-    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        
+
+    func tableView(_ tableView: NSTableView, viewFor _: NSTableColumn?, row: Int) -> NSView? {
         var text = ""
         var cellIdentifier: String = ""
-        
+
         let item = ioDevices[row]
-    
+
         text = item.nameOrAddress
         cellIdentifier = CellIdentifiers.NameCell
-            
+
         if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView {
-            
             cell.textField?.stringValue = text
             return cell
         }
-        
+
         return nil
     }
-    
-    //MARK: IOBluetooth Devices Inquiry Delegate
-    
-    func deviceInquiryStarted(_ sender: IOBluetoothDeviceInquiry!) {
+
+    // MARK: IOBluetooth Devices Inquiry Delegate
+
+    func deviceInquiryStarted(_: IOBluetoothDeviceInquiry!) {
         print("started")
-        
+
         progressIndicator.startAnimation(self)
         progressIndicator.isHidden = false
-        
     }
 
-    func deviceInquiryDeviceFound(_ sender: IOBluetoothDeviceInquiry!, device: IOBluetoothDevice!) {
-        print("found device", device.nameOrAddress)
+    func deviceInquiryDeviceFound(_: IOBluetoothDeviceInquiry!, device: IOBluetoothDevice!) {
+        print("found device", device.nameOrAddress!)
+
         if !ioDevices.contains(device) {
             ioDevices.append(device)
             scrubber.reloadData()
             tableView.reloadData()
         }
-        
     }
 
-    func deviceInquiryComplete(_ sender: IOBluetoothDeviceInquiry!, error: IOReturn, aborted: Bool) {
-        
+    func deviceInquiryComplete(_: IOBluetoothDeviceInquiry!, error _: IOReturn, aborted _: Bool) {
         progressIndicator.stopAnimation(self)
         progressIndicator.isHidden = true
         print("completed")
     }
-    
-    func deviceInquiryDeviceNameUpdated(_ sender: IOBluetoothDeviceInquiry!, device: IOBluetoothDevice!, devicesRemaining: UInt32) {
+
+    func deviceInquiryDeviceNameUpdated(_: IOBluetoothDeviceInquiry!, device _: IOBluetoothDevice!, devicesRemaining _: UInt32) {
         print("name updated")
     }
-    
-    //MARK: Scrubber Delegate and Datasource
-    
-    func numberOfItems(for scrubber: NSScrubber) -> Int {
+
+    // MARK: Scrubber Delegate and Datasource
+
+    func numberOfItems(for _: NSScrubber) -> Int {
         return ioDevices.count
     }
-    
-    func scrubber(_ scrubber: NSScrubber, viewForItemAt index: Int) -> NSScrubberItemView {
 
+    func scrubber(_ scrubber: NSScrubber, viewForItemAt index: Int) -> NSScrubberItemView {
         let itemView = scrubber.makeItem(withIdentifier: scrubberDeviceItemID, owner: nil) as! NSScrubberTextItemView
         guard let name = ioDevices[index].nameOrAddress else { return itemView }
 
         itemView.title = name
-        
+
         if !ioDevices[index].isPaired() {
             itemView.layer?.backgroundColor = NSColor(calibratedRed: 255, green: 0, blue: 0, alpha: 0.5).cgColor
         }
-        
-        if ioDevices[index].isPaired(), !ioDevices[index].isConnected()  {
+
+        if ioDevices[index].isPaired(), !ioDevices[index].isConnected() {
             itemView.layer?.backgroundColor = NSColor.clear.cgColor
         }
-    
+
         if ioDevices[index].isConnected() {
-            itemView.layer?.backgroundColor = NSColor(calibratedRed: 50/255, green: 200/255, blue: 100/255, alpha: 0.5).cgColor
+            itemView.layer?.backgroundColor = NSColor(calibratedRed: 50 / 255, green: 200 / 255, blue: 100 / 255, alpha: 0.5).cgColor
         }
 
         return itemView
     }
-    
-//    Pictures for scrubber. Выглядит пока хреново
-    
-//    func scrubber(_ scrubber: NSScrubber, viewForItemAt index: Int) -> NSScrubberItemView {
-//
-//        let itemView = scrubber.makeItem(withIdentifier: scrubberDeviceItemID, owner: nil) as! NSScrubberImageItemView
-//
-//        if ioDevices[index].nameOrAddress.contains("iPhone") {
-//            itemView.image = #imageLiteral(resourceName: "iphone")
-//        } else if ioDevices[index].nameOrAddress.contains("Mouse") {
-//            itemView.image = #imageLiteral(resourceName: "mouse")
-//        } else if ioDevices[index].nameOrAddress.contains("Keyboard") {
-//            itemView.image = #imageLiteral(resourceName: "keyboard")
-//        } else if ioDevices[index].nameOrAddress.contains("Trackpad") {
-//            itemView.image = #imageLiteral(resourceName: "trackpad")
-//        } else if ioDevices[index].nameOrAddress.contains("Watch") {
-//            itemView.image = #imageLiteral(resourceName: "watch")
-//        } else if ioDevices[index].nameOrAddress.contains("Airpods") {
-//            itemView.image = #imageLiteral(resourceName: "airpods")
-//        }
-//        return itemView
-//    }
-//
-    
+
     func scrubber(_ scrubber: NSScrubber, didHighlightItemAt highlightedIndex: Int) {
-        
         ioBluetoothInquiryManager.stop()
         timer?.invalidate()
-        
+
         if ioDevices[highlightedIndex].isConnected() {
-            
             print("cancel connection")
             ioDevices[highlightedIndex].closeConnection()
             scrubber.itemViewForItem(at: highlightedIndex)?.layer?.backgroundColor = NSColor.clear.cgColor
-            
-        } else if !ioDevices[highlightedIndex].isConnected(), ioDevices[highlightedIndex].isPaired()  {
-            
+
+        } else if !ioDevices[highlightedIndex].isConnected(), ioDevices[highlightedIndex].isPaired() {
             DispatchQueue.global(qos: .background).async {
                 self.ioDevices[highlightedIndex].openConnection()
                 print("connecting to \(self.ioDevices[highlightedIndex].nameOrAddress ?? "no name")")
             }
-            
+
             scrubber.itemViewForItem(at: highlightedIndex)?.isSelected = !(scrubber.itemViewForItem(at: highlightedIndex)?.isSelected)!
-            scrubber.itemViewForItem(at: highlightedIndex)?.layer?.backgroundColor = NSColor(calibratedRed: 50/255, green: 200/255, blue: 100/255, alpha: 0.5).cgColor
-            
+            scrubber.itemViewForItem(at: highlightedIndex)?.layer?.backgroundColor = NSColor(calibratedRed: 50 / 255, green: 200 / 255, blue: 100 / 255, alpha: 0.5).cgColor
+
         } else if !ioDevices[highlightedIndex].isPaired() {
-        
             print("Attempt to pair")
-            
+
             pairingDevice.setDevice(ioDevices[highlightedIndex])
             pairingDevice.start()
-
         }
     }
-    
-    //MARK: Device Pairing delegate
-    
-    func devicePairingStarted(_ sender: Any!) {
+
+    // MARK: Device Pairing delegate
+
+    func devicePairingStarted(_: Any!) {
         print("Pairing started")
     }
-    
-    func devicePairingConnecting(_ sender: Any!) {
+
+    func devicePairingConnecting(_: Any!) {
         print("Connecting")
     }
-    
-    func devicePairingUserPasskeyNotification(_ sender: Any!, passkey: BluetoothPasskey) {
+
+    func devicePairingUserPasskeyNotification(_: Any!, passkey: BluetoothPasskey) {
         print(passkey)
         pairingDevice.replyUserConfirmation(true)
     }
-    
-    func devicePairingPINCodeRequest(_ sender: Any!) {
+
+    func devicePairingPINCodeRequest(_: Any!) {
         print("PIN requested")
     }
-    
-    func deviceSimplePairingComplete(_ sender: Any!, status: BluetoothHCIEventStatus) {
+
+    func deviceSimplePairingComplete(_: Any!, status: BluetoothHCIEventStatus) {
         print(status)
     }
-    
-    func devicePairingFinished(_ sender: Any!, error: IOReturn) {
+
+    func devicePairingFinished(_: Any!, error _: IOReturn) {
         print("finished")
         scrubber.reloadData()
     }
-    
-    //MARK: Misc
-    
-    
+
+    // MARK: Misc
+
     func loadPairedDevices() {
-        
         for device in IOBluetoothDevice.pairedDevices() {
             ioDevices.append(device as! IOBluetoothDevice)
             tableView.reloadData()
             scrubber.reloadData()
         }
-        
+
         if scrubber.numberOfItems == 0 {
             scrubber.showsArrowButtons = false
         } else {
             scrubber.showsArrowButtons = true
         }
-        
     }
-    
-    
+
     @objc func repeatScan() {
         ioBluetoothInquiryManager.clearFoundDevices()
         ioBluetoothInquiryManager.start()
     }
-    
-    //MARK: Actions
-    
-    @IBAction func rescanButtonPressed(_ sender: Any) {
+
+    // MARK: Actions
+
+    @IBAction func rescanButtonPressed(_: Any) {
         print("rescan")
-        
+
         progressIndicator.startAnimation(self)
         progressIndicator.isHidden = false
-    
+
         timer?.fire()
         ioDevices.removeAll()
         repeatScan()
         loadPairedDevices()
-        
+
         tableView.reloadData()
         scrubber.reloadData()
- 
     }
-    
-    @IBAction func rescanUI(_ sender: NSButton) {
+
+    @IBAction func rescanUI(_: NSButton) {
         print("rescan")
-        
+
         progressIndicator.startAnimation(self)
         progressIndicator.isHidden = false
-        
+
         timer?.fire()
         ioDevices.removeAll()
         ioBluetoothInquiryManager.clearFoundDevices()
-        
+
         tableView.reloadData()
         scrubber.reloadData()
-        
+
         repeatScan()
         loadPairedDevices()
     }
-    
-    @IBAction func stopButtonPressed(_ sender: Any) {
+
+    @IBAction func stopButtonPressed(_: Any) {
         timer?.invalidate()
         ioBluetoothInquiryManager.stop()
         progressIndicator.stopAnimation(self)
         progressIndicator.isHidden = true
     }
 }
-
